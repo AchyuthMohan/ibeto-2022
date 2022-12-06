3f3bffb813220a4bd53332de9f95b0c3b3aa22cd
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-12-06 20:41:16 +0530

    changed to final day

--- src/sections/Timeline/Timeline.js
+++ src/sections/Timeline/Timeline.js
@@ -160,7 +160,7 @@ const TimelineComponent = () => {
               className="vertical-timeline-element--work "
               contentStyle={{ color: "#ffff", background: gradient1 }}
               contentArrowStyle={{ borderRight: "7px solid  #094c83" }}
-              date="March"
+              date="Final Day"
             >
               <h3 className="vertical-timeline-element-title">IBeTo Finale</h3>
 
